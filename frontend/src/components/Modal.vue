ce5e290fac883fe39c5ea9fb7e713cc8258a4ffd
rita.radovics <rita.radovics@agillic.com>
2019-05-17 11:22:33 +0200

    MODAL is working

---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,88 @@
+<template>
+<div>
+  <div @click="closeModal" id="greyLayer">
+  </div>
+  <div class="modal">
+    <input type="text" v-model="friend.firstName" placeholder="First name">
+    <input type="text" v-model="friend.lastName" placeholder="Last name">
+    <input type="text" v-model="friend.email" placeholder="Email">
+    <input type="date" v-model="friend.birthday" placeholder="Birthday" title="Birth date">
+    <input type="text" v-model="friend.address" placeholder="Address">
+    <input type="text" v-model="friend.phoneNumber" placeholder="Phone number">
+    <input type="text" v-model="friend.workplace" placeholder="Workplace">
+    <select v-model="friend.category">
+      <option value="1">Family</option>
+      <option value="2">Work</option>
+      <option value="3">Friends</option>
+    </select>
+    <button @click="addData(friend)" class="buttonswithArrow">Add friend<img class="arrow" src='../assets/icons/arrow-right-solid.svg'> </button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  data(){
+    return{
+      friend: {
+        firstName:'',
+        lastName:'',
+        email:'',
+        birthday:'',
+        address:'',
+        phoneNumber:'',
+        workplace:'',
+        category: 1
+      }
+    }
+  },
+  methods:{
+    closeModal(){
+      this.$emit('close')
+    },
+    addData(friend){
+      this.$emit('addData', friend)
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+@import '.././assets/global.stylus.styl'
+
+#greyLayer
+  position fixed
+  top 0
+  left 0
+  width 100%
+  height 100%
+  background-color black
+  opacity 0.7
+  z-index 4
+
+.modal
+  z-index 5
+  display flex
+  flex-flow row wrap
+  align-items center
+  justify-content space-around
+  padding 40px
+  background-color tint(brandPink,90%)
+  position fixed
+  width 600px
+  overflow auto
+  height 70vh
+  top 60px
+  border-radius 25px
+  box-shadow 3px 6px 19px -10px brandPink
+
+  input
+    width 200px
+    margin-bottom 30px
+
+  select
+    width 200px
+    margin-right 10px
+
+</style>
--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,31 +1,14 @@
 <template>
   <div v-if="$root.user" class="dashboard">
-    <div class="panel">
+    <div class="buttonPanel" id="dashboardPanel">
       <button @click="sortingCategory = 1" :class="{activeButton: sortingCategory==1}">Family</button>
       <button @click="sortingCategory = 2" :class="{activeButton: sortingCategory==2}">Work</button>
       <button @click="sortingCategory = 3" :class="{activeButton: sortingCategory==3}">Friends</button>
       <button @click="sortingCategory = 4" :class="{activeButton: sortingCategory==4}">All</button>
       <button  @click="openModal" title="Add friend"><img class="icon" src='../assets/icons/plus-solid.svg'></button>
     </div>
-    <div @click="closeModal" v-if="modalIsOpen==true" class="greyLayer">
-    </div>
-    <div v-if="modalIsOpen==true" class="modalContainer">
-      <div class="modal">
-        <input type="text" v-model="firstName" placeholder="First name">
-        <input type="text" v-model="lastName" placeholder="Last name">
-        <input type="text" v-model="email" placeholder="Email">
-        <input type="date" v-model="birthday" placeholder="Birthday" title="Birth date">
-        <input type="text" v-model="address" placeholder="Address">
-        <input type="text" v-model="phoneNumber" placeholder="Phone number">
-        <input type="text" v-model="workplace" placeholder="Workplace">
-        <select v-model="category">
-          <option value="1">Family</option>
-          <option value="2">Work</option>
-          <option value="3">Friends</option>
-        </select>
-        <button @click="addFriend"> Add friend<img class="arrow" src='../assets/icons/arrow-right-solid.svg'> </button>
-      </div>
-    </div>
+    <Modal v-if="modalIsOpen" @close="closeModal" @addData="addFriend">
+    </Modal>
     <div class="dashboardInner">
       <div @click="goToFriend(friend)" v-for="friend in $root.friends" v-if="friend.category_fk==sortingCategory || sortingCategory==4" class="friendContainer">
         <img v-if="friend.image_url==''" src="../assets/dummy.png">
@@ -38,25 +21,29 @@
 </template>
 
 <script>
+import Modal from '@/components/Modal.vue'
+
 export default {
+  name: 'dashboard',
+  components: {
+    Modal
+  },
   data(){
     return {
-      modalIsOpen: false,
-      firstName:'',
-      lastName:'',
-      email:'',
-      birthday:'',
-      address:'',
-      phoneNumber:'',
-      workplace:'',
-      category: 1,
-      sortingCategory: 4
+      sortingCategory: 4,
+      modalIsOpen: false
     }
   },
   created(){
     this.getAllFriends()
   },
   methods:{
+    openModal(){
+      this.modalIsOpen=true
+    },
+    closeModal(){
+      this.modalIsOpen=false
+    },
     getAllFriends(){
       fetch('/api/api-get-all-friends.php', {
         method: 'GET',
@@ -76,22 +63,16 @@ export default {
       console.log(friend)
       this.$router.push(`friend/${friend.id}`)
     },
-    openModal(){
-      this.modalIsOpen=true
-    },
-    closeModal(){
-      this.modalIsOpen=false
-    },
-    addFriend(){
+    addFriend(friend){
       let formData = new FormData()
-      formData.append('firstName', this.firstName)
-      formData.append('lastName', this.lastName)
-      formData.append('email', this.email)
-      formData.append('birthday', this.birthday)
-      formData.append('address', this.address)
-      formData.append('phoneNumber', this.phoneNumber)
-      formData.append('workplace', this.workplace)
-      formData.append('category', this.category)
+      formData.append('firstName', friend.firstName)
+      formData.append('lastName', friend.lastName)
+      formData.append('email', friend.email)
+      formData.append('birthday', friend.birthday)
+      formData.append('address', friend.address)
+      formData.append('phoneNumber', friend.phoneNumber)
+      formData.append('workplace', friend.workplace)
+      formData.append('category', friend.category)
       fetch('/api/api-add-friend.php', {
         method: 'POST',
         body: formData
@@ -100,14 +81,6 @@ export default {
       .then(json => {
         this.closeModal()
         this.getAllFriends()
-        this.firstName = ''
-        this.lastName = ''
-        this.email = ''
-        this.birthday = ''
-        this.address = ''
-        this.phoneNumber = ''
-        this.workplace = ''
-        this.category = 1
         console.log(json)
       }).catch(error => {
 
@@ -118,20 +91,11 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
 @import '.././assets/global.stylus.styl'
 
-.panel
+#dashboardPanel
   margin-top 80px
-  display flex
-  justify-content space-between
-  align-items flex-start
-
-  button
-    font-size 14px
-    margin-bottom 5px
-    text-transform uppercase
-    color #565051
 
 .dashboardInner
   z-index 0
@@ -159,69 +123,5 @@ export default {
 
   .name
     margin-right 40px
-
-.greyLayer
-  position fixed
-  top 0
-  left 0
-  width 100%
-  height 100%
-  background-color black
-  opacity 0.7
-  z-index 4
-
-.modalContainer
-  z-index 5
-
-.modal
-  z-index 5
-  display flex
-  flex-flow row wrap
-  align-items center
-  justify-content space-around
-  padding 40px
-  background-color tint(brandPink,90%)
-  position fixed
-  width 600px
-  overflow auto
-  height 70vh
-  top 60px
-  border-radius 25px
-  box-shadow 3px 6px 19px -10px brandPink
-
-  input, select
-    padding 15px
-    width 200px
-    margin-bottom 30px
-    border-radius 25px
-    border 3px white solid
-    background-color white
-    outline none
-    box-shadow 3px 6px 19px -10px #ccc
-    color #565051
-
-  select
-    margin-right 10px
-
-  button
-    padding 15px
-    margin-bottom 20px
-    background-color transparent
-    border none
-    color #565051
-    font-weight bold
-    width 50%
-    align-self center
-    cursor pointer
-
-    .arrow
-      width 20px
-      position relative
-      top 8px
-      left 3px
-      color white
-
-::placeholder
-  color #565051
 
 </style>
