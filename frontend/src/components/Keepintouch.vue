bd6507757fb422f8ea2763929fb933c1b1d3ec2e
rita.radovics <rita.radovics@agillic.com>
2019-05-17 15:30:35 +0200

    Keepintouch moved out into a component

---
+++ frontend/src/components/Keepintouch.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="keepInTouch">
+    <div class="imageBox">
+      <img v-if="friend.frequency=='daily'" src="../assets/frequencies/daily.svg">
+      <img v-if="friend.frequency=='weekly'" src="../assets/frequencies/weekly.svg">
+      <img v-if="friend.frequency=='monthly'" src="../assets/frequencies/monthly.svg">
+      <img v-if="friend.frequency=='yearly'" src="../assets/frequencies/yearly.svg">
+      <img v-if="friend.frequency=='never'" src="../assets/frequencies/never.svg">
+    </div>
+    <div class="frequencyBox">
+      <div class="frequencyType" :class="{activeFrequency: friend.frequency=='daily'}" @click="saveStayInTouchData(1, 'daily')">Daily</div>
+      <div class="frequencyType" :class="{activeFrequency: friend.frequency=='weekly'}" @click="saveStayInTouchData(2, 'weekly')">Weekly</div>
+      <div class="frequencyType" :class="{activeFrequency: friend.frequency=='monthly'}" @click="saveStayInTouchData(3, 'monthly')">Monthly</div>
+      <div class="frequencyType" :class="{activeFrequency: friend.frequency=='yearly'}" @click="saveStayInTouchData(4, 'yearly')">Yearly</div>
+      <div class="frequencyType" :class="{activeFrequency: friend.frequency=='never'}" @click="saveStayInTouchData(5, 'never')">Never</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Keepintouch',
+  props: ['friend'],
+  data(){
+    return{
+    }
+  },
+  methods:{
+		saveStayInTouchData(number, frequency){
+			this.$emit('saveStayInTouchData', number, frequency)
+		}
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+@import '.././assets/global.stylus.styl'
+
+.keepInTouch
+	text-align center
+	display flex
+	padding-top 33%
+
+	.imageBox
+		width 70%
+
+		img
+			width 50%
+
+	.frequencyBox
+		display flex
+		flex-direction column
+		justify-content space-around
+		color lightness(brandGrey, 80%)
+
+		.frequencyType
+			cursor pointer
+
+		.activeFrequency
+			color brandPink
+
+</style>
--- frontend/src/pages/Friend.vue
+++ frontend/src/pages/Friend.vue
@@ -1,82 +1,20 @@
 <template>
   <div v-if="friend">
     <div v-if="$root.user" class="friend">
-      <div class="friendAbout" >
-        <div class="friendName">
-          <p v-if="!isEditingFriendAbout">{{friend.first_name}} {{friend.last_name}}</p>
-          <input v-if="isEditingFriendAbout" type="text" v-model="friend.first_name" placeholder="First name">
-          <input id="lastNameInput" v-if="isEditingFriendAbout" type="text" v-model="friend.last_name" placeholder="Last name">
-        </div>
-        <div id="avatarWrapper">
-          <img class="avatar" v-if="friend.image_url==''" src="../assets/dummy.png">
-          <img class="avatar" v-if="friend.image_url !=='' && !friend.image_url.includes('http')" :src="`/uploads/${friend.image_url}`">
-          <img class="avatar" v-if="friend.image_url !=='' &&friend.image_url.includes('http')" :src="friend.image_url">
-          <div :class="[{categoryTitle: true}, friend.category_name]"><p>{{friend.category_name}}</p></div>
-        </div>
-        <div id="fileUploadWrapper">
-        <label for="file">
-        <input type="file" id="file" @change="uploadImage">
-        <img class="icon" title="Change image" src='../assets/icons/upload-solid.svg'>
-        </label>
-        </div>
-        <div>
-          <img class="icon" src='../assets/icons/phone-solid.svg'>
-          <p v-if="!isEditingFriendAbout">{{friend.phone_number}}</p>
-          <input v-if="isEditingFriendAbout" type="text" v-model="friend.phone_number" placeholder="Phone number">
-        </div>
-        <div>
-          <img class="icon" src='../assets/icons/at-solid.svg'>
-          <p v-if="!isEditingFriendAbout">{{friend.email}}</p>
-          <input v-if="isEditingFriendAbout" type="text" v-model="friend.email" placeholder="Email">
-        </div>
-        <div>
-          <img class="icon" src='../assets/icons/map-marker-alt-solid.svg'>
-          <p v-if="!isEditingFriendAbout">{{friend.address}}</p>
-          <input v-if="isEditingFriendAbout" type="text" v-model="friend.address" placeholder="Address">
-        </div>
-        <div>
-          <img class="icon" src='../assets/icons/briefcase-solid.svg'>
-          <p v-if="!isEditingFriendAbout">{{friend.workplace}}</p>
-          <input v-if="isEditingFriendAbout" type="text" v-model="friend.workplace" placeholder="Workplace">
-        </div>
-        <div v-if="!isEditingFriendAbout">
-          <img v-if="!isEditingFriendAbout" class="icon" src='../assets/icons/birthday-cake-solid.svg'>
-          <p v-if="!isEditingFriendAbout">{{getAge(friend.birthdate)}}</p>
-        </div>
-        <div>
-          <img class="icon" src='../assets/icons/baby-carriage-solid.svg'>
-          <p v-if="!isEditingFriendAbout">{{friend.birthdate}}</p>
-          <input v-if="isEditingFriendAbout" type="date" v-model="friend.birthdate" placeholder="Birthday">
-        </div>
-        <div class="editButtonsWrapper">
-          <button v-if="!isEditingFriendAbout" @click="isEditingFriendAbout = true" class="editAboutButton" title="Edit friend info" id="startEditingFriend"><img src='../assets/icons/edit-solid.svg'></button>
-          <button v-if="isEditingFriendAbout" @click="deleteFriend(friend)" id="deleteFriendButton" title="Delete Friend"><img src='../assets/icons/trash-alt-red-solid.svg'></button>
-          <button v-if="isEditingFriendAbout" @click="saveFriendDataChanges(friend)" class="editAboutButton" title="Save friend info" id="saveFriendInfo"><img src='../assets/icons/check-solid.svg'></button>
-        </div>
-      </div>
+      <FriendAbout
+        :friend="friend" @deleteData="deleteFriend" @saveDataChanges="saveFriendDataChanges"
+        @uploadData="uploadImage"
+      >
+      </FriendAbout>
       <div class="friendContent">
-        <div class="friendPanel">
+        <div class="buttonPanel" id="friendPanel">
           <button @click="contentToShow = 1" :class="{activeButton: contentToShow==1}">Stay in touch</button>
           <button @click="contentToShow = 2" :class="{activeButton: contentToShow==2}">Memories</button>
           <button @click="contentToShow = 3" :class="{activeButton: contentToShow==3}">Notes</button>
         </div>
         <div class="contentToShow">
-          <div v-if="contentToShow == 1" class="friendKeepInTouch">
-            <div class="imageBox">
-            <img v-if="friend.frequency=='daily'" src="../assets/frequencies/daily.svg">
-            <img v-if="friend.frequency=='weekly'" src="../assets/frequencies/weekly.svg">
-            <img v-if="friend.frequency=='monthly'" src="../assets/frequencies/monthly.svg">
-            <img v-if="friend.frequency=='yearly'" src="../assets/frequencies/yearly.svg">
-            <img v-if="friend.frequency=='never'" src="../assets/frequencies/never.svg">
-            </div>
-            <div class="frequencyBox">
-              <div class="frequencyType" :class="{activeFrequency: friend.frequency=='daily'}" @click="saveStayInTouch(1, 'daily')">Daily</div>
-              <div class="frequencyType" :class="{activeFrequency: friend.frequency=='weekly'}" @click="saveStayInTouch(2, 'weekly')">Weekly</div>
-              <div class="frequencyType" :class="{activeFrequency: friend.frequency=='monthly'}" @click="saveStayInTouch(3, 'monthly')">Monthly</div>
-              <div class="frequencyType" :class="{activeFrequency: friend.frequency=='yearly'}" @click="saveStayInTouch(4, 'yearly')">Yearly</div>
-              <div class="frequencyType" :class="{activeFrequency: friend.frequency=='never'}" @click="saveStayInTouch(5, 'never')">Never</div>
-            </div>
-          </div>
+          <Keepintouch :friend="friend" @saveStayInTouchData="saveStayInTouch" v-if="contentToShow == 1">
+          </Keepintouch>
           <div v-if="contentToShow == 2" class="memories" >
             <div class="editingMemories">
               <input v-if="isEditingMemory" type="date" v-model="newMemoryDate">
@@ -109,17 +47,23 @@
 </template>
 
 <script>
+import FriendAbout from '@/components/FriendAbout.vue'
+import Keepintouch from '@/components/Keepintouch.vue'
+
 export default {
+  name: 'dashboard',
+  components: {
+    FriendAbout,
+    Keepintouch
+  },
   data () {
     return {
       friend: {
         'image_url': ''
       },
       friendID:'',
-      isEditingFriendAbout: false,
       isEditingNote: false,
       memories: [],
-      imageFuck: '',
       contentToShow: 1,
       isEditingMemory: false,
       newMemoryDate: '',
@@ -228,6 +172,7 @@ export default {
       })
     },
     saveFriendDataChanges(friend){
+			console.log(friend)
       let formData = new FormData()
       formData.append('friendID', friend.friend_fk)
       formData.append('firstName', friend.first_name)
@@ -246,27 +191,12 @@ export default {
       .then(res => res.json())
       .then(json => {
         console.log(json)
-        this.isEditingFriendAbout=false
         //check if its 1
         // this.friend = json.data[0]
       }).catch(error => {
 
         console.log(error)
       })
-    },
-    getAge(dateString){
-      var today = new Date()
-      var birthDate = new Date(dateString)
-      var age = today.getFullYear() - birthDate.getFullYear()
-      var m = today.getMonth() - birthDate.getMonth()
-      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate()))
-      {
-          age--
-      }
-      if(dateString === '0000-00-00'){
-        age = ''
-      }
-      return age
     },
     uploadImage(e){
       let data = new FormData()
@@ -339,174 +269,25 @@ export default {
 <style lang="stylus">
 @import '.././assets/global.stylus.styl'
 
-#startEditingFriend
-  align-self flex-end
-  flex-grow 1
-  text-align right
-
-#saveFriendInfo
-  align-self flex-end
-  flex-grow 1
-  text-align right
-
-#deleteFriendButton
-  border none
-  background transparent
-  cursor pointer
-  outline none
-  margin-bottom 5px
-  padding 0px
-  flex-grow 1
-  text-align left
-
-  img
-    width 15px
-
-#avatarWrapper
-  padding 0px
-  align-self center
-  position relative
-  display grid
-  justify-items center
-  border-radius 50%
-  overflow hidden
-  margin-bottom 5px
-
-  .avatar
-    width 200px
-    height 200px
-    object-fit cover
-    align-self center
-
-  .categoryTitle
-    position absolute
-    width 100%
-    color white
-    bottom 0px
-    text-transform capitalize
-    text-align center
-    color white
-    opacity 0.9
-
-    &.family
-      background-color brandGreen
-
-    &.friends
-      background-color brandPink
-
-    &.work
-      background-color brandGrey
-
-#fileUploadWrapper
-  width 100%
-  height 40px
-  padding 0px
-
-  label
-    width 100%
-    margin-top -50px
-    height 10px
-    position relative
-
-    input
-      width 100%
-      height 30px
-      pointer-events none
-      opacity 0
-      font-size 0
-      padding 0px
-      margin 0px
-      border none
-
-    img
-      position absolute
-      top 20px
-      left 130px
-      cursor pointer
-      width 20px
-      height 20px
-
-.noteButtonsWrapper
-  align-self flex-end
-
-.editButtonsWrapper
-  width 100%
-  margin-right 20px
-  display flex
-  flex-direction row
-  // padding-top 20px
-
-#lastNameInput
-  margin-left 10px
-
-.editAboutButton
-  padding-top 15px
-  border none
-  background transparent
-  cursor pointer
-  outline none
-  margin-bottom 5px
-  padding 0px
-
-  img
-    width 20px
-
-input
-  padding 5px
-  margin-bottom 8px
-  margin-top 20px
-  border-radius 25px
-  border 3px white solid
-  background-color white
-  outline none
-  box-shadow 3px 6px 19px -10px #ccc
-  color #565051
-  align-self center
-
-.friendName
-  font-weight 700
-  font-size 18px
-  align-self center
-
-  input
-    width 70px
-
-.icon
-  width 14px
-  margin-right 10px
-
-.editButton
-  margin-top 40px
+.friendAbout
+  width 40%
 
 .friend
   display flex
-  flex-direction row
   justify-content center
   width 700px
   padding-top 30px
 
-  .friendAbout
-    width 40%
-    display flex
-    flex-direction column
-    align-items flex-start
 
-    >div:nth-child(n+3):nth-last-child(n)
-      display flex
-      flex-direction row
-      align-items center
-      padding-left 20%
-      margin-bottom 10px
-      max-width 70%
+.noteButtonsWrapper
+  align-self flex-end
 
-      >p
-        margin 0px
-        font-size 14px
-        width 80%
-        overflow auto
+// input
+//   padding 5px
+//   margin-bottom 8px
+//   margin-top 20px
+//   align-self center
 
-      >input
-        margin 0
 
   .friendContent
     // margin-top 80px
@@ -514,33 +295,13 @@ input
     display flex
     flex-direction column
 
+    #friendPanel
+      margin-top 20px
+
     .contentToShow
       background-color white
       box-shadow 3px 6px 19px -10px #ccc
       height 100%
-
-      .friendKeepInTouch
-        text-align center
-        display flex
-        flex-direction row
-        padding-top 33%
-
-        .imageBox
-          width 70%
-          img
-            width 50%
-
-        .frequencyBox
-          display flex
-          flex-direction column
-          justify-content space-around
-          color lightness(brandGrey, 80%)
-
-          .frequencyType
-            cursor pointer
-
-          .activeFrequency
-            color brandPink
 
       .memories
         padding 20px 30px 20px 50px
@@ -632,25 +393,4 @@ input
 
         pre
           font-size 14px
-
-    .friendPanel
-      margin-top 20px
-      display flex
-      flex-direction row
-      justify-content space-between
-      align-items flex-start
-
-      button
-        border none
-        background transparent
-        cursor pointer
-        outline none
-        font-family 'Poppins', sans-serif
-        font-size 14px
-        margin-bottom 5px
-        text-transform uppercase
-        color #565051
-
-        .activeButton
-          border-bottom 5px solid brandGreen
 </style>
