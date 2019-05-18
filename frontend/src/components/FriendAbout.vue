59314590d272a4c0d981c3e5e2ddd8bf0c574cb1
rita.radovics <rita.radovics@agillic.com>
2019-05-18 19:34:50 +0200

    friend about correction - finally works

--- frontend/src/components/FriendAbout.vue
+++ frontend/src/components/FriendAbout.vue
@@ -3,7 +3,7 @@
     <div class="friendName">
       <p v-if="!isEditingFriendAbout">{{friend.first_name}} {{friend.last_name}}</p>
       <input v-if="isEditingFriendAbout" type="text" v-model="friend.first_name" placeholder="First name">
-      <input id="lastNameInput" v-if="isEditingFriendAbout" type="text" v-model="friend.last_name" placeholder="Last name">
+      <input v-if="isEditingFriendAbout" type="text" v-model="friend.last_name" placeholder="Last name">
     </div>
     <div class="avatarWrapper">
       <img class="avatar" v-if="friend.image_url==''" src="../assets/dummy.png">
@@ -37,9 +37,9 @@
       <p v-if="!isEditingFriendAbout">{{friend.workplace}}</p>
       <input v-if="isEditingFriendAbout" type="text" v-model="friend.workplace" placeholder="Workplace">
     </div>
-    <div v-if="!isEditingFriendAbout">
-      <img v-if="!isEditingFriendAbout" class="icon2" src='../assets/icons/birthday-cake-solid.svg'>
-      <p v-if="!isEditingFriendAbout">{{getAge(friend.birthdate)}}</p>
+    <div>
+      <img class="icon2" src='../assets/icons/birthday-cake-solid.svg'>
+      <p>{{getAge(friend.birthdate)}}</p>
     </div>
     <div>
       <img class="icon2" src='../assets/icons/baby-carriage-solid.svg'>
@@ -93,137 +93,144 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-@import '.././assets/global.stylus.styl'
+@import '.././assets/variables.stylus.styl'
 
 .friendAbout
-    display flex
-    flex-direction column
-    align-items flex-start
+	display flex
+	flex-direction column
+	align-items flex-start
 
-    >div:nth-child(n+3):nth-last-child(n)
-      display flex
-      align-items center
-      padding-left 20%
-      margin-bottom 10px
-      max-width 70%
+	>div:nth-child(n+3):nth-last-child(n)
+		display flex
+		align-items center
+		padding-left 20%
+		margin-bottom 10px
+		max-width 70%
 
-      >p
-        margin 0px
-        font-size 14px
-        width 80%
-        overflow auto
+		>p
+			margin 0px
+			height 20px
+			font-size 14px
+			width 80%
+			overflow auto
 
-      >input
-        padding 5px
-        margin-bottom 8px
-        margin-top 20px
-        align-self center
-        margin 0
+		>input
+			height 16px
+			padding 2px
+			margin-bottom 8px
+			margin-top 20px
+			align-self center
+			margin 0
+			border none
+			font-size 14px
 
-    .friendName
-      font-weight 700
-      font-size 18px
-      align-self center
+	.friendName
+		font-weight 700
+		font-size 18px
+		align-self center
 
-      input
-        width 70px
-    
-      #lastNameInput
-        margin-left 10px
+		input
+			width 70px
+			padding 6.1px
+			border none
+			margin 15.5px 10px 15.5px 0px
+			align-self center
+			font-size 18px
+			font-weight 700
+			height 20px
 
-    .avatarWrapper
-      align-self center
-      position relative
-      display grid
-      justify-items center
-      border-radius 50%
-      overflow hidden
-      margin-bottom 10px
+	.avatarWrapper
+		align-self center
+		position relative
+		display grid
+		justify-items center
+		border-radius 50%
+		overflow hidden
+		margin-bottom 10px
 
-      .avatar
-        width 200px
-        height 200px
-        object-fit cover
-        align-self center
+		.avatar
+			width 200px
+			height 200px
+			object-fit cover
+			align-self center
 
-      .categoryTitle
-        position absolute
-        width 100%
-        color white
-        bottom 0px
-        text-transform capitalize
-        text-align center
-        color white
-        opacity 0.9
+		.categoryTitle
+			position absolute
+			width 100%
+			color white
+			bottom 0px
+			text-transform capitalize
+			text-align center
+			color white
+			opacity 0.9
 
-        &.family
-          background-color brandGreen
+			&.family
+				background-color brandGreen
 
-        &.friends
-          background-color brandPink
+			&.friends
+				background-color brandPink
 
-        &.work
-          background-color brandGrey
+			&.work
+				background-color brandGrey
 
-    #fileUploadWrapper
-      width 100%
-      height 40px
-      padding 0px
+	#fileUploadWrapper
+		width 100%
+		height 40px
+		padding 0px
 
-      label
-        width 100%
-        margin-top -50px
-        height 10px
-        position relative
+		label
+			width 100%
+			margin-top -50px
+			height 10px
+			position relative
 
-        input
-          width 100%
-          height 30px
-          pointer-events none
-          opacity 0
-          font-size 0
-          padding 0px
-          margin 0px
-          border none
+			input
+				width 100%
+				height 30px
+				pointer-events none
+				opacity 0
+				font-size 0
+				padding 0px
+				margin 0px
+				border none
 
-        img
-          position absolute
-          top 20px
-          left 130px
-          cursor pointer
-          width 20px
-          height 20px
+			img
+				position absolute
+				top 20px
+				left 130px
+				cursor pointer
+				width 20px
+				height 20px
 
-    .editButtonsWrapper
-      width 100%
-      margin-right 20px
-      display flex
+	.editButtonsWrapper
+		width 100%
+		margin-right 20px
+		display flex
 
-      .editAboutButton
-        padding-top 15px
-        margin-bottom 5px
-        padding 0px
+		.editAboutButton
+			padding-top 15px
+			padding 0px
 
-        img
-          width 20px
+			img
+				width 20px
 
-      #deleteFriendButton
-        margin-bottom 5px
-        padding 0px
-        flex-grow 1
-        text-align left
+		#deleteFriendButton
+			margin-bottom 5px
+			padding 0px
+			flex-grow 1
+			text-align left
 
-        img
-          width 15px
+			img
+				width 15px
 
-      #saveFriendInfo
-        align-self flex-end
-        flex-grow 1
-        text-align right
+		#saveFriendInfo
+			align-self flex-end
+			flex-grow 1
+			text-align right
 
-      #startEditingFriend
-        align-self flex-end
-        flex-grow 1
-        text-align right
+		#startEditingFriend
+			align-self flex-end
+			flex-grow 1
+			text-align right
 
 </style>
