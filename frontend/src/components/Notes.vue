23f8edb7bf8bbdd5ad048260473dd88a72bb329a
rita.radovics <rita.radovics@agillic.com>
2019-05-19 10:37:37 +0200

    notes styling fixed

--- frontend/src/components/Notes.vue
+++ frontend/src/components/Notes.vue
@@ -1,8 +1,12 @@
 <template>
 	<div  class="note">
 		<div class="noteButtonsWrapper">
-			<button v-if="!isEditingNote" @click="startEditingNote" title="Edit notes"><img src='../assets/icons/edit-solid.svg'></button>
-			<button v-if="isEditingNote" @click="saveData" title="Save changes"><img src='../assets/icons/check-solid.svg'></button>
+			<div v-if="!isEditingNote">
+			<button @click="startEditingNote" title="Edit notes"><img src='../assets/icons/edit-solid.svg'></button>
+			</div>
+			<div v-if="isEditingNote">
+			<button @click="saveData" title="Save changes"><img src='../assets/icons/check-solid.svg'></button>
+			</div>
 		</div>
 		<pre v-if="!isEditingNote">{{ notes }}</pre>
 		<textarea ref="noteArea" v-if="isEditingNote" v-model="note" placeholder="Notes to remember"></textarea>
@@ -42,19 +46,30 @@ export default {
 .note
 	display flex
 	flex-direction column
-	padding 30px 30px
+	padding 20px 30px
 	height 100%
 
 	.noteButtonsWrapper
 		align-self flex-end
+		display flex
+		width 100%
+		height 30px
+		justify-content flex-end
+		margin-top 10px
+		margin-bottom 10px
+		align-items center
 
-		button
+		div
 			width 30px
 			height 30px
+			text-align right
 
-			img
-				width 20px
-				height 20px
+			button
+				padding 0px
+
+				img
+					width 22px
+					height 22px
 
 	textarea
 		border none
@@ -62,8 +77,12 @@ export default {
 		outline none
 		font-size 14px
 		height 100%
+		margin 0px
+		padding 0px
 
 	pre
 		font-size 14px
+		margin 0px
+		padding 0px
 
 </style>
